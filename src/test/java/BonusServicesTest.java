e495ded949997a754d90f556b96ebd48961f98eb
alexander test <martunov2013@mail.ru>
2024-02-27 15:48:39 +0300

    Added programm and test

---
+++ src/test/java/BonusServicesTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+public class BonusServicesTest {
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusServices service = new BonusServices();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusServices service = new BonusServices();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
